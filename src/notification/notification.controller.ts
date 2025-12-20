2e1c34aa60e8c9aebf997f601069e3a594a1307d
vansh <vanshsin784@gmail.com>
2025-12-20 13:21:19 +0530

    fix: guard in notification controller

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,34 +1,28 @@
-import {
-  Controller,
-  Get,
-  Patch,
-  Param,
-  Req,
-  UseGuards,
-} from "@nestjs/common";
-import { NotificationService } from "./notification.service";
-import { JwtAuthGuard } from "../common/jwt.guard"; // adjust path if needed
+import { Controller, Get, Patch, Param, Req, UseGuards } from '@nestjs/common';
+import { NotificationService } from './notification.service';
+import { JwtAuthGuard } from '../common/jwt.guard'; // adjust path if needed
+import { SessionDto } from 'src/common/dto';
 
-@Controller("notifications")
+@Controller('notifications')
 @UseGuards(JwtAuthGuard)
 export class NotificationController {
-  constructor(private readonly service: NotificationService) {}
+    constructor(private readonly service: NotificationService) {}
 
-  @Get("unread")
-  getUnread(@Req() req) {
-    // 🔑 User info comes from req.session (set by JwtAuthGuard)
-    const userId = req.session?.sub || req.session?._id;
-    return this.service.getUnread(userId);
-  }
+    @Get('unread')
+    getUnread(@Req() req: { sessionDto: SessionDto }) {
+        // 🔑 User info comes from req.session (set by JwtAuthGuard)
+        const userId = req?.sessionDto?.user_id;
+        return this.service.getUnread(userId);
+    }
 
-  @Patch("read/:id")
-  markRead(@Param("id") id: string) {
-    return this.service.markAsRead(id);
-  }
+    @Patch('read/:id')
+    markRead(@Param('id') id: string) {
+        return this.service.markAsRead(id);
+    }
 
-  @Patch("read-all")
-  markAll(@Req() req) {
-    const userId = req.session?.sub || req.session?._id;
-    return this.service.markAllAsRead(userId);
-  }
+    @Patch('read-all')
+    markAll(@Req() req: { sessionDto: SessionDto }) {
+        const userId = req?.sessionDto?.user_id;
+        return this.service.markAllAsRead(userId);
+    }
 }
